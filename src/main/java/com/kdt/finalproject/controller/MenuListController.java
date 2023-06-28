d24972d096ab13e975073b58e4ac5aebc31934e8
gjisoo <gjisoo@gmail.com>
2023-06-28 14:31:23 +0900

    menuinfo controller 수정

--- src/main/java/com/kdt/finalproject/controller/MenuListController.java
+++ src/main/java/com/kdt/finalproject/controller/MenuListController.java
@@ -160,26 +160,4 @@ public class MenuListController {
         return mv;
     }
 
-    @RequestMapping("/menu/info")
-    public ModelAndView menuInfo(String foodCost, String foodNm, String foodMaterial, String etc, String f_image,
-            String RestNm, String r_photo) {
-
-        ModelAndView mv = new ModelAndView();
-
-        mv.addObject("RestNm", RestNm);
-        mv.addObject("foodCost", foodCost);
-        mv.addObject("foodNm", foodNm);
-        mv.addObject("foodMaterial", foodMaterial);
-        mv.addObject("etc", etc);
-
-        if (f_image != null)
-            mv.addObject("f_image", f_image);
-        if (r_photo != null)
-            mv.addObject("r_photo", r_photo);
-
-        mv.setViewName("menuInfo");
-
-        return mv;
-    }
-
 }
