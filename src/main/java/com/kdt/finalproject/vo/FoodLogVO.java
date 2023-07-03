4d253d68b4dd8e7fe6718e264b25b51f19bd4144
junsuk1226 <junsuk1226@naver.com>
2023-07-03 15:49:40 +0900

    VO추가

---
+++ src/main/java/com/kdt/finalproject/vo/FoodLogVO.java
@@ -0,0 +1,16 @@
+package com.kdt.finalproject.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class FoodLogVO {
+    private String fl_idx, foodCost, foodlogcol, foodNm, foodMaterial, etc, stdRestNm, bestfoodyn, premiumyn,
+            recommendyn,
+            seasonMenu, seq, f_image, editDate, f_idx, fl_var1, fl_var2, fl_var3, fl_var4, fl_var5;
+}
