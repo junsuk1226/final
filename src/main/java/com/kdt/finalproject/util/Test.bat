af5ca7fd0a2a9879492a20e2eb94f0f1895ada34
hahaha3456 <hahaha3456@naver.com>
2023-07-18 10:44:34 +0900

    장바구니 수정

--- src/main/java/com/kdt/finalproject/util/Test.bat
+++ src/main/java/com/kdt/finalproject/util/Test.bat
@@ -3,5 +3,5 @@ chcp 65001
 cd\
 cd Mywork\finalproject\final-5\src\main\java\com\kdt\finalproject\util\
 start javac -encoding utf-8 -classpath jpos113-controls.jar;jcl.jar JposPrinterReceiptTest.java
-start java -classpath .;JposPrinterJavaPOS.jar;jpos113-controls.jar;jcl.jar;xercesimpl.jar;xml-apis.jar JposPrinterReceiptTest.java 죽전(서울)휴게소 돈까스/뚝배기불고기 9000/10000 1/3 39000 0066
+start java -classpath .;JposPrinterJavaPOS.jar;jpos113-controls.jar;jcl.jar;xercesimpl.jar;xml-apis.jar JposPrinterReceiptTest.java 죽전(서울)휴게소 갈비탕/도토리묵밥/돌솥비빔밥 14000/7500/9000 2/1/1 44500 0068
 pause
